f6d82bcdde958312080751b8457ba270d914c5dc
PEDRO COSTA CORDEIRO <cordeiro.pedro@aluno.senac.rio>
2026-07-08 21:34:38 -0300

    primeiro envio de arquivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EpiList } from './epi-list/epi-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EpiList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('seguranca-frontend');
+  // protected readonly title = signal('Turma 2026.1');
+  titulo = "Sistema de Controle de EPIs";
+  subtitulo = "Gestão de Segurança do Trabalho";
+  descricao = "Aplicação para controle, monitoramento e conformidade dos equipamentos de proteção individual";
 }
